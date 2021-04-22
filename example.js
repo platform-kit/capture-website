7963a0177818cb4ce71bf91230293c518d670f79
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2021-04-22 15:48:50 -0700

    Using headless chrome for Netlify/AWS Lambda

--- example.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const captureWebsite = require('.');
-
-// Run this file with `$ node example.js`
-
-(async () => {
-	await captureWebsite.file('https://sindresorhus.com', 'example.png');
-})();
